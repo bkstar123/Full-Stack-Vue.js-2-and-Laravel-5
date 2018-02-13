c33b4359c58c2fa1ffc1f2b938c2dbb1c38dbb40
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2018-02-13 17:51:18 +0700

    .

--- vuebnb/resources/assets/js/store.js
+++ vuebnb/resources/assets/js/store.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from './router';
+import axios from 'axios';
 
 Vue.use(Vuex);
+
+axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': window.csrf_token
+};
 
 export default new Vuex.Store({
 	state: {
@@ -39,5 +45,16 @@ export default new Vuex.Store({
                 state.listing_summaries = data.listings;
             } 
         }
+    },
+    actions: {
+        toggleSaved({ commit, state }, id) {
+            if (state.auth) {
+                axios.post('/api/user/toggle_saved', { id }).then(
+                    () => commit('toggleSaved', id)
+                );
+            } else {
+                router.push('/login');
+            }
+        } 
     }
 });
